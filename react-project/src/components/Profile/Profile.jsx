79e4f5035522a7b95c339d25b27e9339858b19fd
ekaz85 <57419605+ekaz85@users.noreply.github.com>
2021-07-09 16:44:22 -0400

    break into components

--- react-project/src/components/Profile/Profile.jsx
+++ react-project/src/components/Profile/Profile.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import classes from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
-    return           <div className={classes.content}>
+    return (
+        <div className={classes.content}>
         <div>
             <img src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=6&m=825383494&s=612x612&w=0&h=pamh6qxyNPCnNAVru4BrAHt2qTHAGCD9lDiN_6MbaNY=" alt=""/>
         </div>
         <div>
             ava + description
         </div>
-        <div>
-            My post
-            <div>
-                New post
-            </div>
-            <div>
-                <div className={classes.item}>
-                    post 1
-                </div>
-                <div className={classes.item}>
-                    post 2
-                </div>
-            </div>
-        </div>
+        <MyPosts />
     </div>
+    )
 }
 
 export default Profile;
