8c289fdc71038a9c8ac75a2c4f25bd63a4e5876b
ekaz85 <57419605+ekaz85@users.noreply.github.com>
2021-07-09 23:26:46 -0400

    Dialogs added

--- react-project/src/components/Profile/Profile.jsx
+++ react-project/src/components/Profile/Profile.jsx
@@ -4,15 +4,17 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={classes.content}>
         <div>
-            <img src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=6&m=825383494&s=612x612&w=0&h=pamh6qxyNPCnNAVru4BrAHt2qTHAGCD9lDiN_6MbaNY=" alt=""/>
+            <div>
+                <img
+                    src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=6&m=825383494&s=612x612&w=0&h=pamh6qxyNPCnNAVru4BrAHt2qTHAGCD9lDiN_6MbaNY="
+                    alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts/>
         </div>
-        <div>
-            ava + description
-        </div>
-        <MyPosts />
-    </div>
     )
 }
 
