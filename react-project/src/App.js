ac559e8692d63fff47e4c4504be8b52a075c957f
ekaz85 <57419605+ekaz85@users.noreply.github.com>
2021-08-06 00:03:23 -0400

    start FLUX 33

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -8,6 +8,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
+import {addPost} from "./redux/state";
 
 const App = (props) => {
 
@@ -26,7 +27,9 @@ const App = (props) => {
                     <Route path='/dialogs'
                            render={ () => <Dialogs state={props.state.dialogsPage} /> }/>
                     <Route path='/profile'
-                           render={ () => <Profile state={props.state.profilePage} /> }/>
+                           render={ () => <Profile
+                               state={props.state.profilePage}
+                               addPost={props.addPost}/> }/>
                 </div>
             </div>
         </BrowserRouter>
--- react-project/src/redux/state.js
+++ react-project/src/redux/state.js
@@ -1,3 +1,5 @@
+import {rerenderEntireTree} from "../render";
+
 let state = {
     profilePage: {
         posts : [
@@ -24,7 +26,19 @@ let state = {
             {id: 4, message: 'Yo'},
             {id: 5, message: 'Yo'}
         ]
-    }
+    },
+    sidebar: { }
+}
+
+export let addPost = (postMessage) => {
+    let newPost = {
+        id: 5,
+        message: postMessage,
+        likesCount: 0
+    };
+
+    state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 }
 
 export default state;
