0eeeabd7454161be1a1e7ad1ce6994ce72963a52
ekaz85 <57419605+ekaz85@users.noreply.github.com>
2021-08-06 02:13:44 -0400

    FLUX-cycles 34

--- react-project/src/redux/state.js
+++ react-project/src/redux/state.js
@@ -7,8 +7,8 @@ let state = {
             {id: 2, message: 'It\'s my first post', likesCount: 11},
             {id: 2, message: 'Blabla', likesCount: 11},
             {id: 2, message: 'Dada', likesCount: 11}
-        ]
-
+        ],
+        newPostText: 'it-kam.com'
     },
     dialogsPage: {
         dialogs : [
@@ -30,14 +30,21 @@ let state = {
     sidebar: { }
 }
 
-export let addPost = (postMessage) => {
+window.state = state;
+
+export let addPost = () => {
     let newPost = {
         id: 5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount: 0
     };
-
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
+    rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
 
