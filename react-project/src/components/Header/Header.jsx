8be86f64feb747fbc46aa71bfba2039b1ed8efa9
ekaz85 <57419605+ekaz85@users.noreply.github.com>
2021-08-16 21:18:16 -0400

    css

--- react-project/src/components/Header/Header.jsx
+++ react-project/src/components/Header/Header.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import classes from './Header.module.css';
 
 const Header = () => {
-    return <header className={classes.header}>
-        <img src="https://cdn.dribbble.com/users/13604/screenshots/7557893/image.png?compress=1&resize=400x300" alt=""/>
+    return (
+        <header className={classes.header}>
+                <img src="https://cdn.dribbble.com/users/13604/screenshots/7557893/image.png?compress=1&resize=400x300" alt=""/>
+                        <ul>
+                            <li><a href="#"><i className="fa">Menu</i></a></li>
+                            <li><a href="#"><i className="fa">Projects</i></a></li>
+                            <li><a href="#"><i className="fa">About Us</i></a></li>
+                            <li><a href="#"><i className="fa">Gallery</i></a></li>
+                            <li><a href="#"><i className="fa">Contacts</i></a></li>
+                        </ul>
         </header>
+    );
 }
 
 export default Header;
--- react-project/src/components/Navbar/Navbar.jsx
+++ react-project/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import classes from './Navbar.module.css';
 import {NavLink} from "react-router-dom";
+import anime from 'animejs/lib/anime.es.js';
+
+// const container = document.querySelector('#container');
+// for (var i = 1; i <= 100; i++) {
+//     const blocks = document.createElement('div');
+//     blocks.classList.add('block');
+//     container.appendChild(blocks);
+// }
+//
+// function generate() {
+//     anime({
+//         targets: '.block',
+//         translateX: function () {
+//             return anime.random(-700, 700)
+//         },
+//         translateY: function () {
+//             return anime.random(-700, 700)
+//         },
+//         scale: function () {
+//             return anime.random(1, 5)
+//         }
+//     })
+// }
+
+// generate();
 
 const Navbar = () => {
     return <nav className={classes.nav}>
