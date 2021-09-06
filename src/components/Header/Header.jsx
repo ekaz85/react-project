76c7e38363170cc3129a257d4bd3bf9a0d1cf1d7
ekaz85 <57419605+ekaz85@users.noreply.github.com>
2021-09-06 17:45:32 -0400

    cleanup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import classes from './Header.module.css';
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <header className={classes.header}>
-                <img src="https://cdn.dribbble.com/users/13604/screenshots/7557893/image.png?compress=1&resize=400x300" alt=""/><h1>Teammate</h1>
-                        <ul>
-                            <li><a href="#"><i className="fa">Menu</i></a></li>
-                            <li><a href="#"><i className="fa">Projects</i></a></li>
-                            <li><a href="#"><i className="fa">About Us</i></a></li>
-                            <li><a href="#"><i className="fa">Gallery</i></a></li>
-                            <li><a href="#"><i className="fa">Contacts</i></a></li>
-                        </ul>
+            <div className={classes.upperheader}>
+                {/* <img src="https://cdn.dribbble.com/users/13604/screenshots/7557893/image.png?compress=1&resize=400x300" alt=""/> */}
+                <h1>Teammates</h1>
+            </div>
+            <div className={classes.lowerheader}>
+                    <NavLink to="/profile" activeClassName={classes.activeLink}>Profile</NavLink>
+                    <NavLink to="/dialogs" activeClassName={classes.activeLink}>Message</NavLink>
+                    <NavLink to="/news" activeClassName={classes.activeLink}>News</NavLink>
+                    <NavLink to="/music" activeClassName={classes.activeLink}>Music</NavLink>
+                    <NavLink to="/settings" activeClassName={classes.activeLink}>Settings</NavLink>
+            </div>
         </header>
     );
 }
 
 export default Header;
+
+ {/* <img src="https://cdn.dribbble.com/users/13604/screenshots/7557893/image.png?compress=1&resize=400x300" alt=""/> */}
